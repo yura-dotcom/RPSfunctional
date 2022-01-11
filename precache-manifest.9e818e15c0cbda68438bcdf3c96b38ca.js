844c1dd2256881737017fadb2cb2740187656033
master <devgeorg222@gmail.com>
2022-01-11 13:25:18 +0200

    Updates

---
+++ precache-manifest.9e818e15c0cbda68438bcdf3c96b38ca.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9703a8e6911f3bda351d0d646dd35800",
+    "url": "/RPSfunctional/index.html"
+  },
+  {
+    "revision": "f7badbee9b64ea904fdd",
+    "url": "/RPSfunctional/static/css/main.c17a1eaf.chunk.css"
+  },
+  {
+    "revision": "38b63845285aa863597e",
+    "url": "/RPSfunctional/static/js/2.b95c2202.chunk.js"
+  },
+  {
+    "revision": "3405545c67c2b04913629256d4fb5d95",
+    "url": "/RPSfunctional/static/js/2.b95c2202.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f7badbee9b64ea904fdd",
+    "url": "/RPSfunctional/static/js/main.1d76f3ba.chunk.js"
+  },
+  {
+    "revision": "be95a6e82cfa14625e5a",
+    "url": "/RPSfunctional/static/js/runtime-main.3d512c47.js"
+  }
+]);
